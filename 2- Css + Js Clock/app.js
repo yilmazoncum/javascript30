c2b20a3046d1ef0493e2b2bbe66f181415343b3e
Öncüm Korkmaz Yılmaz <oncumkorkmaz@gmail.com>
2021-12-03 13:33:25 +0300

    add a dot to centre of clock

--- 2- Css + Js Clock/app.js
+++ 2- Css + Js Clock/app.js
@@ -3,18 +3,16 @@ const minHand = document.getElementById("min-hand");
 const hourHand = document.getElementById("hour-hand");
 
 function setDate() {
-    const now = new Date ;
-  
-    secRotate = ((now.getSeconds())*6); //6 degree comes from 360deg/60second
-    secHand.style.transform = `rotate(${secRotate}deg)`;
+  const now = new Date();
 
-    minRotate = ((now.getMinutes())*6);//6 degree comes from 360deg/60minutes
-    minHand.style.transform = `rotate(${minRotate}deg)`;
+  secRotate = now.getSeconds() * 6; //6 degree comes from 360deg/60second
+  secHand.style.transform = `rotate(${secRotate}deg)`;
 
-    hourRotate =((now.getHours())*30); // 30degree comes from 360deg/12hours
-    hourHand.style.transform = `rotate(${hourRotate}deg)`;
+  minRotate = now.getMinutes() * 6; //6 degree comes from 360deg/60minutes
+  minHand.style.transform = `rotate(${minRotate}deg)`;
 
+  hourRotate = now.getHours() * 30; // 30degree comes from 360deg/12hours
+  hourHand.style.transform = `rotate(${hourRotate}deg)`;
 }
 
 setInterval(setDate, 1000);
-     
