9d6aecc883da2e22b79ced84b778f065f0f718c0
Öncüm Korkmaz Yılmaz <oncumkorkmaz@gmail.com>
2021-12-03 20:50:23 +0300

    make cosmetics changes

--- 3- Update Css Variables with JS/app.js
+++ 3- Update Css Variables with JS/app.js
@@ -3,15 +3,17 @@ const spacing = document.getElementById("spacing");
 const blur = document.getElementById("blur");
 const mainDiv = document.querySelector(".main");
 const photoDiv = document.querySelector(".photo");
+const headerDiv = document.querySelector(".headJS");
 
 color.addEventListener("change",()=>{
-    console.log(color.value);
     mainDiv.style.backgroundColor = color.value;
+    headerDiv.style.color = `${color.value}`;
 })
+
 spacing.addEventListener("change",()=>{
-    console.log(spacing.value);
     photoDiv.style.backgroundSize =`${50*(spacing.value/100)}em ${30*(spacing.value/100)}em`;
 })
+
 blur.addEventListener("change",()=>{
       photoDiv.style.filter =`blur(${blur.value/10}px)`;
 })
