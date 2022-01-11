eefbf23d9d6bcb3bdc061a239fddb9db5996ea9d
yilmazoncum <oncumkorkmaz@gmail.com>
2022-01-11 13:47:33 +0300

    add changable background according to video

--- 11- Custom HTML5 Video Player/app.js
+++ 11- Custom HTML5 Video Player/app.js
@@ -5,12 +5,39 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+const body = document.querySelector('body');
+
+function changeBackgroundLoop() {
+    setInterval(changeBackground,1000);
+}
+
+function changeBackground(params) {
+    console.log("slm");
+
+const canvas = document.createElement("canvas");
+// scale the canvas accordingly
+canvas.width = video.videoWidth;
+canvas.height = video.videoHeight;
+
+// draw the video at that frame
+var ctx = canvas.getContext('2d');
+ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+// convert it to a usable data URL
+
+//const dataURL = canvas.toDataURL();
+console.log(ctx);
+console.log(canvas.width);
+var data = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+console.log(data);
+
+body.style.background = "rgb(" + data[60] + "," + data[61] + "," + data[62] + ")";
+}
 
 function togglePlay() {
     if (video.paused) {
         video.play();
     }
-    else{
+    else {
         video.pause();
     }
 }
@@ -18,6 +45,7 @@ function togglePlay() {
 function updateButton() {
     const icon = this.paused ? '►' : '❚ ❚';
     toggle.textContent = icon;
+    changeBackgroundLoop.call();
 }
 
 function skip() {
@@ -31,31 +59,32 @@ function handleRangeUpdate() {
 
 function handleRangeUpdate() {
     video[this.name] = this.value;
-  }
-  
-  function handleProgress() {
+}
+
+function handleProgress() {
     const percent = (video.currentTime / video.duration) * 100;
     progressBar.style.flexBasis = `${percent}%`;
-  }
-  
-  function scrub(e) {
+}
+
+function scrub(e) {
     const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
     video.currentTime = scrubTime;
-  }
-  
+}
 
-  video.addEventListener('click', togglePlay);
-  video.addEventListener('play', updateButton);
-  video.addEventListener('pause', updateButton);
-  video.addEventListener('timeupdate', handleProgress);
-  
-  toggle.addEventListener('click', togglePlay);
-  skipButtons.forEach(button => button.addEventListener('click', skip));
-  ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
-  ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
-  
-  let mousedown = false;
-  progress.addEventListener('click', scrub);
-  progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
-  progress.addEventListener('mousedown', () => mousedown = true);
-  progress.addEventListener('mouseup', () => mousedown = false);
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+
