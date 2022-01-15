ac3a1cb7040aa56b9d14ed15e8c7ba67e971fba8
yilmazoncum <oncumkorkmaz@gmail.com>
2022-01-15 19:38:10 +0300

    add effect decision

--- 19 - Webcam Fun/app.js
+++ 19 - Webcam Fun/app.js
@@ -3,101 +3,112 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+const radios = document.querySelectorAll('input[type=radio]');
 
 function getVideo() {
-    navigator.mediaDevices.getUserMedia({video:true,audio:false})
-        .then(localMediaStream => {
-            console.log(localMediaStream);
-            video.srcObject = localMediaStream;
-            video.play();
-        })
-        .catch(err => {
-            console.error('noo!',err);
-        })
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+      video.srcObject = localMediaStream;
+      video.play();
+    })
+    .catch(err => {
+      console.error('noo!', err);
+    })
 }
 
 
 function paintToCanvas() {
-    const width = video.videoWidth;
-    const height = video.videoHeight;
-    canvas.width = width;
-    canvas.height = height;
-  
-    return setInterval(() => {
-      ctx.drawImage(video, 0, 0, width, height);
-      let pixels = ctx.getImageData(0, 0, width, height);
-      
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
 
-     pixels = redEffect(pixels);
-  
-      //pixels = rgbSplit(pixels);
-      //ctx.globalAlpha = 0.8;
-  
-      //pixels = greenScreen(pixels);
-      
-      ctx.putImageData(pixels, 0, 0);
-    }, 16);
-  }
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0, 0, width, height);
+
+    let effect = effectDecision();
+
+    if (effect == "red") {
+      pixels = redEffect(pixels);
+    }
+    else if(effect == "split"){
+      pixels = rgbSplit(pixels);
+      ctx.globalAlpha = 0.8;
+    }
+    else if (effect == "green") {
+      pixels = greenScreen(pixels);
+    }
+
+    ctx.putImageData(pixels, 0, 0);
+  }, 16);
+}
 
 function takePhoto() {
-    snap.currentTime = 0;
-    snap.play();
+  snap.currentTime = 0;
+  snap.play();
 
-    const data = canvas.toDataURL('image/jpeg');
-    const link = document.createElement('a');
-    link.href = data;
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
 
-    link.setAttribute('download','image');
-    link.innerHTML = `<img src="${data}" alt="image" />`;
-    strip.insertBefore(link,strip.firstChild);
+  link.setAttribute('download', 'image');
+  link.innerHTML = `<img src="${data}" alt="image" />`;
+  strip.insertBefore(link, strip.firstChild);
 
 }
 
 function redEffect(pixels) {
-    for (let i = 0; i < pixels.data.length; i+=4) {
-      pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
-      pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
-      pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
-    }
-    return pixels;
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
   }
-  
-  function rgbSplit(pixels) {
-    for (let i = 0; i < pixels.data.length; i+=4) {
-      pixels.data[i - 150] = pixels.data[i + 0]; // RED
-      pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
-      pixels.data[i - 550] = pixels.data[i + 2]; // Blue
-    }
-    return pixels;
-  }
-  
-  function greenScreen(pixels) {
-    const levels = {};
-  
-    document.querySelectorAll('.rgb input').forEach((input) => {
-      levels[input.name] = input.value;
-    });
-  
-    for (i = 0; i < pixels.data.length; i = i + 4) {
-      red = pixels.data[i + 0];
-      green = pixels.data[i + 1];
-      blue = pixels.data[i + 2];
-      alpha = pixels.data[i + 3];
-  
-      if (red >= levels.rmin
-        && green >= levels.gmin
-        && blue >= levels.bmin
-        && red <= levels.rmax
-        && green <= levels.gmax
-        && blue <= levels.bmax) {
-        // take it out!
-        pixels.data[i + 3] = 0;
-      }
-    }
-  
-    return pixels;
-  }
-  
+  return pixels;
+}
 
-getVideo();
-video.addEventListener('canplay',paintToCanvas);
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0]; // RED
+    pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+    pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+  }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
+}
+
+function effectDecision() {
+  for (let i = 0; i < radios.length; i++) {
+    if(radios[i].checked) return radios[i].value;
+  }
+}
+
+  getVideo();
+  video.addEventListener('canplay', paintToCanvas);
